23eb999a1ab1582231da9e505a068b3736fac0e2
AndreasKG <andreaskruse@hotmail.dk>
2024-10-09 14:20:26 +0200

    updated test code again

--- test.py
+++ test.py
@@ -20,6 +20,8 @@ pwm2F = 8
 pwm1B = 7
 pwm2B = 1
 
+pwm = GPIO.PWM(pwm1F, 50)
+pwm.start(0)
 
 # Setting pins to outpt
 GPIO.setup(dir1F, GPIO.OUT)
@@ -31,8 +33,17 @@ GPIO.setup(pwm2F, GPIO.OUT)
 GPIO.setup(pwm1B, GPIO.OUT)
 GPIO.setup(pwm2B, GPIO.OUT)
 
-pwm = GPIO.PWM(pwm1F, 50)
-pwm.start(0)
+def forward(direction, speed):
+    if direction == 'forward':
+        GPIO.output(dir1F, GPIO.HIGH)
+        GPIO.output(dir2F, GPIO.LOW)
+        GPIO.output(dir1B, GPIO.HIGH)
+        GPIO.output(dir2B, GPIO.LOW)
+    pwm.ChangeDutyCycle(speed)
+
+
+forward('forward', 50)
 time.sleep(5)
+
 pwm.stop()
 GPIO.cleanup()
