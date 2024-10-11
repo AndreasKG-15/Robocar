0e79ff9532945bbbe8495481146e00ad2e60cfb9
tobiashafjall <toha7@student.eadania.dk>
2024-10-11 09:54:18 +0200

    fjernstyrstest

--- fjernstyringtest.py
+++ fjernstyringtest.py
@@ -1,50 +1,69 @@
+import RPi.GPIO as GPIO
 import time
 from sshkeyboard import listen_keyboard
 
+# Opsætning af GPIO pins
+GPIO.setmode(GPIO.BCM)
+
+# Motor pins
+dir1F = 14
+dir2F = 23
+dir1B = 15
+dir2B = 24
+pwm1F = 25
+pwm2F = 8
+pwm1B = 7
+pwm2B = 1
+
+# Opsætning af pins til output
+GPIO.setup(dir1F, GPIO.OUT)
+GPIO.setup(dir2F, GPIO.OUT)
+GPIO.setup(dir1B, GPIO.OUT)
+GPIO.setup(dir2B, GPIO.OUT)
+GPIO.setup(pwm1F, GPIO.OUT)
+GPIO.setup(pwm2F, GPIO.OUT)
+GPIO.setup(pwm1B, GPIO.OUT)
+GPIO.setup(pwm2B, GPIO.OUT)
+
+# PWM opsætning
+pwm1 = GPIO.PWM(pwm1F, 50)
+pwm2 = GPIO.PWM(pwm2F, 50)
+pwm3 = GPIO.PWM(pwm1B, 50)
+pwm4 = GPIO.PWM(pwm2B, 50)
+
+# Start PWM
+pwm1.start(0)
+pwm2.start(0)
+pwm3.start(0)
+pwm4.start(0)
+
 active_keys = set()
 
-def kør_fremad():
-    print("Bilen kører fremad!")
-
-def kør_bagud():
-    print("Bilen kører tilbage!")
-
-def drej_venstre():
-    print("Bilen drejer til venstre!")
-
-def drej_højre():
-    print("Bilen drejer til højre!")
+def Move(state, speedLeft, speedRight):
+    GPIO.output(dir1F, state)
+    GPIO.output(dir2F, state)
+    GPIO.output(dir1B, state)
+    GPIO.output(dir2B, state)
+    pwm1.ChangeDutyCycle(speedRight)
+    pwm2.ChangeDutyCycle(speedLeft)
+    pwm3.ChangeDutyCycle(speedRight)
+    pwm4.ChangeDutyCycle(speedLeft)
 
 def update_actions():
     if 'w' in active_keys:
-        kør_fremad()
+        Move(GPIO.LOW, 40, 40)
     if 's' in active_keys:
-        kør_bagud()
+        Move(GPIO.LOW, 0, 0)
     if 'a' in active_keys:
-        drej_venstre()
+        Move(GPIO.LOW, 0, 30)
     if 'd' in active_keys:
-        drej_højre()
-
+        Move(GPIO.LOW, 30, 0)
 
 def press(key):
     active_keys.add(key)
     update_actions()
-    
 
-#    if key == 'w':
-#        kør_fremad()
-#        print("'w' pressed: Kører fremad")
-#    elif key == 's':
-#        kør_bagud()
-#        print("'s' pressed: Kører bagud")
-#    elif key == 'd':
-#        drej_højre()
-#        print("'d' pressed: Drej til højre")
-#    elif key == 'a':
-#        drej_venstre()
-#        print("'a' pressed: Drej til venstre")
     
-
 def release(key):
     active_keys.discard(key)
     update_actions()
@@ -60,3 +79,11 @@ try:
         time.sleep(0.1)
 except KeyboardInterrupt:
     print("Programmet stoppes.")
+
+finally:
+    print("Rydder op")
+    pwm1.stop()
+    pwm2.stop()
+    pwm3.stop()
+    pwm4.stop()
+    GPIO.cleanup()
